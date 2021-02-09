a7f4d69a628d4a4b4101c2d0e71142e60f98fbf2
Abd. Asis <id.abdasis@gmail.com>
2021-02-09 23:51:25 +0700

    update data survey kondiis

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -5,36 +5,107 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Document</title>
+    <style>
+        p{
+            font-size: 10px;
+        }
+        td{
+            font-size: 8px;
+        }
+        table{
+            width: 100%;
+        }
+        *{
+            margin: 0px;
+            padding: 2px;
+        }
+        .border{
+            border: 1px dashed #444;
+        }
+        .text-center{
+            text-align: center;
+        }
+
+        .text-right{
+            text-align: right !important;
+        }
+    </style>
 </head>
 
 <body>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-4 shadow-sm">
-                <h3 class="text-center text-primary">PMB AIR BERSIH</h3>
-                <h5 class="text-center">Tagihan : {{ date('d-F-Y') }}</h5>
-            </div>
-        </div>
-        <p>ID <span class="float-right">{{ $id }}</span></p>
-        <p>Nomor Refrensi: <span class="float-right">{{ date('dmy') . $id }}</span></p>
-        <hr>
-        <p>Nama <span class="float-right">{{ $sumber_dana }}</span></p>
-        <p>Alamat <span class="float-right">{{ $alamat }}</span></p>
-        <p>No Meter <span class="float-right">{{ $no_meter }}</span></p>
-        <p>Meter Awal: <span class="float-right">{{ $meter_awal }}</span></p>
-        <p>Meter Akhir: <span class="float-right">{{ $meter_akhir }}</span></p>
-        <p>Jumlah Pekaian: <span class="float-right">{{ $meter_akhir-$meter_awal }}</span></p>
-        <p>Biaya Perawatan: <span class="float-right">Rp. {{ number_format($biaya_perawatan,2,',','.') }}</span></p>
-        <p>Harga Perkubik: <span class="float-right">Rp. {{ number_format($harga_perkubik,2,',','.') }}</span></p>
-        <hr>
-        <p>Diskon: <span class="float-right">Rp. {{ number_format($diskon,2,',','.') }}</span></p>
-        <p>Total: <span class="float-right">Rp. {{ number_format($total,2,',','.') }}</span></p>
-
-
+<div class="row justify-content-center">
+    <div class="col-md-4 shadow-sm">
+        <p class="text-center">PMB AIR BERSIH</p>
+        <p class="text-center">Tagihan : {{ date('d-F-Y') }}</p>
     </div>
+</div>
+<div class="border"></div>
+
+<table>
+    <tr>
+        <td>ID</td>
+        <td>:</td>
+        <td class="text-right"> {{ $id }}</td>
+    </tr>
+    <tr>
+        <td>Nomor Refrensi</td>
+        <td>:</td>
+        <td class="text-right">{{ date('dmy') . $id }}</td>
+    </tr>
+
+    <tr>
+        <td>Nama</td>
+        <td>:</td>
+        <td class="text-right">{{ $sumber_dana }}</td>
+    </tr>
+    <tr>
+        <td>Alamat</td>
+        <td>:</td>
+        <td class="text-right">{{ $alamat }}</td>
+    </tr>
+    <tr>
+        <td>No Meter</td>
+        <td>:</td>
+        <td class="text-right">{{ $no_meter }}</td>
+    </tr>
+    <tr>
+        <td>Meter Awal</td>
+        <td>:</td>
+        <td class="text-right">{{ $meter_awal }}</td>
+    </tr>
+    <tr>
+        <td>Meter Akhir</td>
+        <td>:</td>
+        <td class="text-right">{{ $meter_akhir }}</td>
+    </tr>
+    <tr>
+        <td>Jumlah Pekaian</td>
+        <td>:</td>
+        <td class="text-right">{{ ($meter_akhir-$meter_awal) }}</td>
+    </tr>
+    <tr>
+        <td>Biaya Perawatan</td>
+        <td>:</td>
+        <td class="text-right">Rp. {{ number_format($biaya_perawatan,2,',','.') }}</td>
+    </tr>
+    <tr>
+        <td>Harga Perkubik</td>
+        <td>:</td>
+        <td class="text-right">Rp. {{ number_format($harga_perkubik,2,',','.') }}</td>
+    </tr>
+    <tr>
+        <td>Diskon</td>
+        <td>:</td>
+        <td class="text-right">Rp. {{ number_format($diskon,2,',','.') }}</td>
+    </tr>
+    <tr>
+        <td>Total</td>
+        <td>:</td>
+        <td class="text-right">Rp. {{ number_format($total,2,',','.') }}</td>
+    </tr>
+</table>
+<div class="border"></div>
 </body>
 
 </html>
